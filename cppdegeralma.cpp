f5244c916f435baf53314bd456ccd301affc7396
Süleyman Toklu <99734183+Tiheli@users.noreply.github.com>
2022-10-22 11:09:47 +0300

    Add files via upload

---
+++ cppdegeralma.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	string isim1;
+	cout << "isminizi giriniz.." <<endl;
+
+	cin >> isim1;
+
+	cout<< " merhaba, {"<< isim1<<"}"<<endl;
+	
+	system("pause");
+	return 0;
+		
+}
+
